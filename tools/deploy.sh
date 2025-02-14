1d09df2ffb27834960abba8211e7900ccfd56081
lillianphyo <khinpyaephyosan@gmail.com>
2025-02-14 16:34:04 +0630

    feat: update posts

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -50,7 +50,7 @@ build() {
   if [[ -d $SITE_DIR ]]; then
     rm -rf "$SITE_DIR"
   fi
-
+  bundle lock --add-platform x86_64-linux
   # build
   JEKYLL_ENV=production bundle exec jekyll b -d "$SITE_DIR$_baseurl" --config "$_config"
 }
