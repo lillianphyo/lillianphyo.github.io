94e9aefe29a5d9abad4567aaa86a401be45b5dab
lillianphyo <khinpyaephyosan@gmail.com>
2025-09-27 03:37:29 +0900

    Remove NextAuth.js dependencies and fix CLIENT_FETCH_ERROR
    - Removed SessionProvider from providers.tsx
    - Deleted auth.ts configuration file
    - Removed next-auth, @auth/prisma-adapter, @prisma/client, and prisma dependencies
    - Cleaned up unused authentication code
    - Re-enabled Giscus comments without NextAuth conflicts

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from './theme-provider'
 import { I18nProvider } from './i18n-provider'
 
@@ -10,17 +9,15 @@ interface ProvidersProps {
 
 export function Providers({ children }: ProvidersProps) {
   return (
-    <SessionProvider>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="light"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <I18nProvider>
-          {children}
-        </I18nProvider>
-      </ThemeProvider>
-    </SessionProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <I18nProvider>
+        {children}
+      </I18nProvider>
+    </ThemeProvider>
   )
 }
